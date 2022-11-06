f697dd0a74ebd7ec460400acf188f8e16740612f
sujay2306 <sujayks007@gmail.com>
2022-11-06 15:25:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+
+app.get("/myget", (req, res) => { 
+    console.log(req.body);
+
+    res.send(req.body)
+})
+
+
+app.listen(4000, () => {
+    console.log("running server at 4000");
+})
+
